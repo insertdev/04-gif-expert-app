c2b1b175d340652bfa2423d6918efc5cec8d1f64
Iván Sanz <ivansanz@joumma.com>
2021-02-10 21:25:17 +0100

    links

--- src/index.js
+++ src/index.js
@@ -9,3 +9,10 @@ ReactDOM.render(
   <GifExpertApp />,
   document.getElementById('root')
 );
+
+
+/*
+Links
+https://es.reactjs.org/docs/faq-structure.html
+https://hackernoon.com/structuring-projects-and-naming-components-in-react-1261b6e18d76
+*/
