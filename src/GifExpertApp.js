f89881ef55a9b0a1398c7b00fa41020d3abba805
Iván Sanz <ivansanz@joumma.com>
2021-02-10 21:24:49 +0100

    GifExpertApp - Componente

---
+++ src/GifExpertApp.js
@@ -0,0 +1,10 @@
+import React, {useState} from 'react';
+
+export const GifExpertApp = () => {
+    return (
+        <>
+            <h2>GifExpertApp</h2>
+            <hr />
+        </>
+    );
+}
