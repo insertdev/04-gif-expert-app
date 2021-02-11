957a6530af72f21471e22837e218796e1454d93a
Iván Sanz <ivansanz@joumma.com>
2021-02-11 20:56:22 +0100

    componente AddCategory

--- src/GifExpertApp.js
+++ src/GifExpertApp.js
@@ -1,20 +1,21 @@
 import React, {useState} from 'react';
+import {AddCategory} from './components/AddCategory';
+
+
 
 export const GifExpertApp = () => {
-    // const categories = ['One Punch', 'Samurai X', 'Dragon Ball'];
     const [categories, setCategories] = useState(['One Punch', 'Samurai X', 'Dragon Ball']);
 
-    const handleAdd = () => {
-        // valores anteriores más el nuevo
-        setCategories([...categories, 'HunterXHunter']);
-    }
+    // const handleAdd = () => {
+    //     // valores anteriores más el nuevo
+    //     setCategories([...categories, 'HunterXHunter']);
+    // }
 
     return (
         <>
             <h2>GifExpertApp</h2>
+            <AddCategory />
             <hr />
-
-            <button onClick={handleAdd}>Agregar</button>
 
             <ol>
                 {
