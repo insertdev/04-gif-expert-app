5a0110609d952e58d6299a332267104921e96a0e
Iván Sanz <ivansanz@joumma.com>
2021-02-11 21:46:59 +0100

    clases css

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -29,16 +29,18 @@ export const GifGrid = ({ category }) => {
     }
 
     return (
-        <div>
-            <h3>{category}</h3>     
-            {
-                images.map( img => (
-                <GifGridItem 
-                    key={img.id}
-                    { ...img }
-                />
-                ))       
-            }  
-        </div>
+        <>
+            <h3>{category}</h3>
+            <div className="card-grid">
+                {
+                    images.map(img => (
+                        <GifGridItem
+                            key={img.id}
+                            {...img}
+                        />
+                    ))
+                }
+            </div>
+        </>
     )
 }
