bc23f5533c250585cec4246a75a7fbf497b70adc
Iván Sanz <ivansanz@joumma.com>
2021-02-12 22:52:48 +0100

    useFetchGifs: obtener y mostrar cargando

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,29 +1,16 @@
 import React from 'react'
 import { useFetchGifs } from '../hooks/useFetchGifs'
-// import { GifGridItem } from './GifGridItem';
-// import { getGifs } from '../helpers/getGifs';
+import { GifGridItem } from './GifGridItem';
 
 export const GifGrid = ({ category }) => {
-
-    // const [images, setImages] = useState([]);
-    const {loading} = useFetchGifs();
-    
-
-    
-
-    // // para que solo se ejecute una vez
-    // // el array es necesario
-    // useEffect(() => {
-    //     getGifs(category)
-    //         .then(setImages);
-    // }, [category])
+    const { data:images, loading } = useFetchGifs(category);
 
     return (
         <>
             <h3>{category}</h3>
 
-            { loading ? 'Cargando...' : 'Data cargada' }
-            {/* <div className="card-grid">
+            { loading && <p>Loading</p> }
+            <div className="card-grid">
                 {
                     images.map(img => (
                         <GifGridItem
@@ -32,7 +19,7 @@ export const GifGrid = ({ category }) => {
                         />
                     ))
                 }
-            </div>  */}
+            </div>
         </>
     )
 }
