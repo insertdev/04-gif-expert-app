e59a9094f8aca13a8db2be71bc2910d81718a03a
Iván Sanz <ivansanz@joumma.com>
2021-02-16 22:01:46 +0100

    evaluar si existen componentes

--- src/tests/components/GifGrid.test.js
+++ src/tests/components/GifGrid.test.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import '@testing-library/jest-dom';
 import { shallow } from 'enzyme';
 import { GifGrid } from '../../components/GifGrid';
 import { useFetchGifs } from '../../hooks/useFetchGifs';
@@ -24,7 +25,7 @@ describe('pruebas en <GifGrid />', () => {
             id: 'ABC',
             url: 'https://localhost/cualquier/cosa.jpg',
             title: 'Cualquier cosa'
-        }]
+        }];
 
         useFetchGifs.mockReturnValue({
             data: gifs,
@@ -32,8 +33,10 @@ describe('pruebas en <GifGrid />', () => {
         });
         
         const wrapper = shallow(<GifGrid category={category} />);
-        
+
         expect(wrapper).toMatchSnapshot();
+        // expect(wrapper.find('p').exists() ).toBe(false);
+        expect(wrapper.find('GifGridItem').length ).toBe(gifs.length);
 
     })
     
