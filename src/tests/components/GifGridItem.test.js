6a004490781f2803420e2005b47cf5514a641900
Iván Sanz <ivansanz@joumma.com>
2021-02-16 19:05:57 +0100

    mas pruebas en GifGridItem

--- src/tests/components/GifGridItem.test.js
+++ src/tests/components/GifGridItem.test.js
@@ -5,10 +5,32 @@ import { GifGridItem } from '../../components/GifGridItem';
 describe('Pruebas en <GifGridItem />', () => {
     const title = 'Un título';
     const url = 'https://localhost/algo.jpg'
+    const wrapper = shallow(<GifGridItem title={title} url={url} />);
 
-    test('debe de mostrar el componente correctamente', () => {
-        const wrapper = shallow(<GifGridItem title={title} url={url} />);
-        
+    test('debe de mostrar el componente correctamente', () => {      
         expect(wrapper).toMatchSnapshot();
     })    
+
+    test('debe de tener un párrafo con el title', () => {
+        const p = wrapper.find('p');
+        expect(p.text().trim()).toBe(title);
+    })
+
+    test('debe de tener la imagen igual al url y alt de los props', () => {
+        const img = wrapper.find('img');
+        // console.log(img.props('src'));
+        expect(img.prop('src')).toBe(url);
+        expect(img.prop('alt')).toBe(title);
+    })
+
+    test('debe de tener animate__bounceIn', () => {
+        const div = wrapper.find('div');
+        // console.log(div.prop('className'));
+        const className = div.prop('className');
+
+        expect(className.includes('animate__bounceIn')).toBe(true);
+    })
+    
+    
+    
 })
